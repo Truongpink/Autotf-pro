c040545067616cf1f6bc5463e75fd987ec62b466
Truongpink <143158107+Truongpink@users.noreply.github.com>
2024-06-16 11:11:18 +0700

    Update TF_keys.js

--- TF_keys.js
+++ TF_keys.js
@@ -1,15 +1,4 @@
-/*
-Script Author: DecoAri
-Fix Author: Hely-T
-Reference: https://raw.githubusercontent.com/Hely-T/TestFlight-All/master/Resource/Script/TestFlight/TF_keys.js
-Các bước sử dụng cụ thể
-- Nhập Plugin
-- Vào trang Mitm để kích hoạt và bật Mitm qua Http2
-- Vào Profile -> Data Persistence -> Import Data by key. Key= APP_ID, Value= (can be left blank)
-- Khởi động bật Loon và cho phép VPN, sau đó vào App TestFlight để kích hoạt Plugin
-- Đi tới Profile -> Plugin -> Chọn vào Script, trong mục APP_ID điền ID của TF bạn muốn tham gia làm giá trị, (ID là ký tự sau khi nối liên kết https://testflight.apple.com/join/LPQmtkUs String (nghĩa là "LPQmtkUs" trong ví dụ này)⚠️: hỗ trợ liên kết TF không giới hạn, mỗi liên kết cần phân tách bằng dấu phẩy "," (chẳng hạn như: LPQmtkUs ,Hgun65jg,8yhJgv)
-)
-*/
+
 if (APP_ID) {
     console.log("Extracted APP_ID:", APP_ID);
 } else {
