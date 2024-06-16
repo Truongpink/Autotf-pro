8381e01b6bb14255c6900ee655ee92d73c833354
Truongpink <143158107+Truongpink@users.noreply.github.com>
2024-06-16 16:25:22 +0700

    Update TF_keys.js

--- TF_keys.js
+++ TF_keys.js
@@ -1,51 +1,16 @@
-
-if (APP_ID) {
-    console.log("Extracted APP_ID:", APP_ID);
+$persistentStore.write(null, 'request_id')
+let url = $request.url
+let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, '$2')
+let session_id = $request.headers['x-session-id'] || $request.headers['X-Session-Id']
+let session_digest = $request.headers['x-session-digest'] || $request.headers['X-Session-Digest']
+let request_id = $request.headers['x-request-id'] || $request.headers['X-Request-Id']
+$persistentStore.write(key, 'key')
+$persistentStore.write(session_id, 'session_id')
+$persistentStore.write(session_digest, 'session_digest')
+$persistentStore.write(request_id, 'request_id')
+if ($persistentStore.read('request_id') !== null) {
+  $notification.post('Vui lòng đóng tập lệnh này', 'Thông tin thu được thành công','')
 } else {
-    console.log("Không tìm thấy APP_ID trong URL.");
+  $notification.post('Không thể lấy được thông tin','Vui lòng mở MITM, Chuyển đổi H2 và thêm testflight.apple.com','')
 }
-
-// Các dòng mã khác trong script testflight.js có thể ở đây...
-const reg1 = /^https:\/\/testflight\.apple\.com\/v3\/accounts\/(.*)\/apps$/;
-const reg2 = /^https:\/\/testflight\.apple\.com\/join\/(.*)/;
-if (reg1.test($request.url)) {
-    $persistentStore.write(null, 'request_id')
-    let url = $request.url
-    let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, '$2')
-    let session_id = $request.headers['X-Session-Id'] || $request.headers['x-session-id']
-    let session_digest = $request.headers['X-Session-Digest'] || $request.headers['x-session-digest']
-    let request_id = $request.headers['X-Request-Id'] || $request.headers['x-request-id']
-    let ua = $request.headers['User-Agent'] || $request.headers['user-agent']
-    $persistentStore.write(key, 'key')
-    $persistentStore.write(session_id, 'session_id')
-    $persistentStore.write(session_digest, 'session_digest')
-    $persistentStore.write(request_id, 'request_id')
-    $persistentStore.write(ua, 'tf_ua')
-    console.log($request.headers)
-    if ($persistentStore.read('request_id') !== null) {
-      $notification.post('TF thu thập thông tin', 'Thu thập thông tin thành công, vui lòng đóng tập lệnh!','')
-
-    } else {
-      $notification.post('Thu thập thông tin TF', 'Việc thu thập thông tin không thành công, vui lòng bật chuyển đổi Mitm qua HTTP2 và khởi động lại VPN và Ứng dụng TestFlight! ','')
-    }
-    $done({})
-}
-if (reg2.test($request.url)) {
-  let appId = $persistentStore.read("APP_ID");
-  if (!appId) {
-    appId = "";
-  }
-  let arr = appId.split(",");
-  const id = reg2.exec($request.url)[1];
-  arr.push(id);
-  arr = unique(arr).filter((a) => a);
-  if (arr.length > 0) {
-    appId = arr.join(",");
-  }
-  $persistentStore.write(appId, "APP_ID");
-  $notification.post("TestFlight Tự động tham gia", `APP_ID đã được thêm: ${id}`, `ID hiện tại: ${appId}`);
-  $done({})
-}
-function unique(arr) {
-  return Array.from(new Set(arr));
-}
+$done({})
