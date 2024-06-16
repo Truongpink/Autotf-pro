d1ecfe47fd49b397231bb67352d3537fabe00fb2
Truongpink <143158107+Truongpink@users.noreply.github.com>
2024-06-16 15:02:35 +0700

    Update Auto_join_TF1.js

--- Tool/Surge5/Auto_join_TF1.js
+++ Tool/Surge5/Auto_join_TF1.js
@@ -76,16 +76,6 @@ if (typeof $request !== 'undefined' && $request) {
         }
     })()
 }
-
-async function autoPost(ID, ids) {
-    let Key = $persistentStore.read('key')
-    let testurl = `https://testflight.apple.com/v3/accounts/${Key}/join/${appId}`;
-   let header = {
-    'X-Session-Id': `${$persistentStore.read('session_id')}`,
-    'X-Session-Digest': `${$persistentStore.read('session_digest')}`,
-    'X-Request-Id': `${$persistentStore.read('request_id')}`,
-    'User-Agent': `${$persistentStore.read('tf_ua')}`,
-  }
 async function autoPost(ID, ids) {
     let Key = $persistentStore.read('key')
     let testurl = `https://testflight.apple.com/v3/accounts/${Key}/ru/`
